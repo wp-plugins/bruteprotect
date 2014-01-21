2f7528f2bff5d2431cb16bec4a4de2d10f1d9546
samhotchkiss <samhotchkiss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-21 20:34:21 +0000

    version fix
    git-svn-id: https://plugins.svn.wordpress.org/bruteprotect/trunk@842647 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bruteprotect.php
+++ bruteprotect.php
@@ -6,7 +6,7 @@
 Plugin Name: BruteProtect
 Plugin URI: http://bruteprotect.com/
 Description: BruteProtect allows the millions of WordPress bloggers to work together to defeat Brute Force attacks. It keeps your site protected from brute force security attacks even while you sleep. To get started: 1) Click the "Activate" link to the left of this description, 2) Sign up for a BruteProtect API key, and 3) Go to your BruteProtect configuration page, and save your API key.
-Version: 1.1
+Version: 1.1.1
 Author: Hotchkiss Consulting Group
 Author URI: http://hotchkissconsulting.com/
 License: GPLv2 or later
@@ -28,7 +28,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-define('BRUTEPROTECT_VERSION', '1.1');
+define('BRUTEPROTECT_VERSION', '1.1.1');
 define('BRUTEPROTECT_PLUGIN_URL', plugin_dir_url( __FILE__ ));
 
 if ( is_admin() ) :
